ddab2f6c2185c2b95cb88e1e406262097e2e993f
gururaj02 <ngururaj58@gmail.com>
2026-05-27 23:47:00 +0530

    Update InventoryClient.java

--- ecom-order-service/src/main/java/com/example/ecom_order_service/client/InventoryClient.java
+++ ecom-order-service/src/main/java/com/example/ecom_order_service/client/InventoryClient.java
@@ -8,7 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+/*
 @FeignClient(name = "inventory-service", url = "http://localhost:8081", configuration = InventoryFeignClientConfig.class)
+*/
+@FeignClient(name = "ecom-inventory-service", configuration = InventoryFeignClientConfig.class)
 public interface InventoryClient {
 
     @GetMapping("/inventory/{productId}")
